41bae3edb98f210f68329d441471551d111efe0e
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-05-21 23:04:37 +0100

    Stop the Iframe in restaurant cards from being clicked and directing to google maps. Internal link for entire card.

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -7,18 +7,33 @@ import CardFooter from "react-bootstrap/CardFooter";
 
 import styles from "./RestaurantCard.module.css";
 
-const RestaurantCard = ({ name, slug, restType, location, mapembed, cuisine }) => {
+const RestaurantCard = ({
+    name,
+    slug,
+    restType,
+    location,
+    mapembed,
+    cuisine,
+}) => {
     return (
         <Link href={`/restaurants/${slug}`}>
             <Card className={`my-3 ${styles.RestCard}`}>
-                <CardHeader className={`text-center`}><p className={`m-0 ${styles.RestName}`}>{name}</p></CardHeader>
+                <CardHeader className={`text-center`}>
+                    <p className={`m-0 ${styles.RestName}`}>{name}</p>
+                </CardHeader>
                 <CardBody className={`text-center`}>
-                    <p className={`m-0 ${styles.RestDetails}`}>{restType} restaurant in {location}</p>
+                    <p className={`m-0 ${styles.RestDetails}`}>
+                        {restType} restaurant in {location}
+                    </p>
                     <p className={`m-0`}>Serving {cuisine} cuisines</p>
-                    <p className={`m-0 ${styles.CTA}`}>Click for more info <i className="bi bi-hand-index-thumb"></i></p>
+                    <p className={`m-0 ${styles.CTA}`}>
+                        Click for more info{" "}
+                        <i className="bi bi-hand-index-thumb"></i>
+                    </p>
                 </CardBody>
                 <CardFooter className="text-center p-0">
                     <iframe
+                        className={styles.IframeNoClick}
                         title={`Map showing the location of ${name}, in ${location}, Manchester`}
                         width="100%"
                         height="auto"
