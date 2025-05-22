127c63d35773f90fb2a04be927d2e32448b1570d
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-05-23 00:13:50 +0100

    Change google analytics scripts in the layout.js file.

--- app/layout.js
+++ app/layout.js
@@ -56,27 +56,25 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                {/* Google Tag Manager */}
-                <Script id="gtm-head" strategy="afterInteractive">
-                    {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-59ZJT4K2');
-                    `}
-                </Script>
+               {/* Google Analytics Script */}
+                <Script
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-QY3SPPS6BJ"
+                />
+                <Script
+                    id="gtag-init"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-QY3SPPS6BJ');
+                        `,
+                    }}
+                />
             </head>
             <body className={`${roboto.variable} ${dancingScript.variable}`}>
-                 {/* Google Tag Manager (noscript) */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-59ZJT4K2"
-                        height="0"
-                        width="0"
-                        style={{ display: "none", visibility: "hidden" }}
-                    ></iframe>
-                </noscript>
                 <SiteNavbar />
                 {children}
                 <SiteFooter />
